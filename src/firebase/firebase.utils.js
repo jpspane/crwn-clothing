4b70a42dcdd7262193a1fe8c5e91b8b4a1242b31
jspane2000 <jpspane@gmail.com>
2020-08-31 10:06:40 -0700

    basic auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -21,6 +21,32 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
+
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    // Add to firestore
+
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.error("Error create user", error);
+    }
+  }
+
+  return userRef;
+};
 
 export default firebase;
